700850dba92380807398158237c9a6a4522affef
Santiago <santiago@billie.io>
2019-02-22 12:30:35 +0100

    using filter from protopretzel

--- prototest/plumber.R
+++ prototest/plumber.R
@@ -1,7 +1,5 @@
 library(plumber)
-library(RProtoBuf)
-
-readProtoFiles(here::here("prototest.proto"))
+library(protopretzel)
 
 serializer_protobuf <- function() {
   function(val, req, res, errorHandler) {
@@ -20,35 +18,6 @@ addSerializer("Protobuf", serializer_protobuf)
 
 api <- plumber$new(here::here("prototest/api.R"))
 
-api$filter("protobuf_filter", function(req) {
-  protobuf_header <- setNames(
-    as.list(stringr::str_match(
-      req$HEADERS["content-type"],
-      stringr::regex("application/x-protobuf;\\s*messagetype=([\\w\\.]+)")
-    )),
-    c("content-type", "message-type")
-  )
-  
-  protobuf_message_type <- protobuf_header[["message-type"]]
-
-  if (!is.na(protobuf_message_type)) {
-    
-    # rook.input contains the incoming request as a strem, so we need to rewind
-    # it first to get the value
-    req$rook.input$rewind()
-  
-    req$protobuf <- setNames(
-      list(
-        RProtoBuf::read(
-          get(protobuf_message_type), 
-          req$rook.input$read()
-          )
-        ), 
-      protobuf_message_type
-    )
-  }
-
-  forward()
-})
+addProtobufFilter(api, descriptor_path = here::here("prototest.proto"))
 
 api$run(host = "0.0.0.0", port = 8000)
