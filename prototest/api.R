900e8dc225508f60aed9c4f2fb72a47985d41aa3
Santiago <santiago@billie.io>
2019-02-26 16:42:24 +0100

    add nested endpoint

--- prototest/api.R
+++ prototest/api.R
@@ -7,12 +7,28 @@
 #    https://www.rplumber.io/
 #
 
-#* @apiTitle Plumber+Protobuf Example API
+#* @apiTitle Plumber+ProtBuf Example API
 
 #* Echo back the input
 #* @param req The API request
-#* @serializer Protobuf
+#* @serializer protoBuf
 #* @post /echo
 function(req) {
+  browser()
   protoplumb.TestPayload$new(a = req$protobuf$protoplumb.TestPayload$a)
+}
+
+#* Echo back the input
+#* @param req The API request
+#* @serializer protoBuf
+#* @post /nestedres
+function(req) {
+  #browser()
+  protoplumb.NestedPayload$new(test = 
+                                 list(new(protoplumb.TestPayload,
+                                          a = req$protobuf$protoplumb.TestPayload$a),
+                                      new(protoplumb.TestPayload,
+                                          a = req$protobuf$protoplumb.TestPayload$a)
+                                 )
+  )
 }
